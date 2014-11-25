53251e8204c72b7859ad2ded8de86ebd5990d22c
lkokemohr <lars@lkokemohr.de>
2014-11-25 15:16:32 +0100

    Version vom Ende der ersten Stunde

---
+++ Physikdemo/Assets/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+	// Beschleunigung in x-, y- und z-Richtung
+	public float accelerationX;
+	public float accelerationY;
+	public float accelerationZ;
+
+	// Geschwindigkeit in x-, y- und z-Richtung
+	private float velocityX = 0;
+	private float velocityY = 0;
+	private float velocityZ = 0;
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		float speedDiffX = accelerationX * Time.deltaTime;
+		velocityX = velocityX + speedDiffX;
+
+		// Bewegung = Zeitdifferenz * Geschwindigkeit
+		// s = v * t
+		float deltaX = velocityX * Time.deltaTime;
+		float deltaY = velocityY * Time.deltaTime;
+		float deltaZ = velocityZ * Time.deltaTime;
+
+		// Translate = Bewege um ...
+		this.transform.Translate(deltaX, deltaY, deltaZ);
+	}
+}
+
