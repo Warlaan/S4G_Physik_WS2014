acf8f90454d5a67797b39895ac4852603e329dcc
lkokemohr <lars@lkokemohr.de>
2014-11-25 15:49:11 +0100

    Die Werte für die Geschwindigkeit und Beschleunigung sind jetzt in Vektoren gespeichert (statt in einzelnen Kommazahlen)

--- Physikdemo/Assets/Movement.cs
+++ Physikdemo/Assets/Movement.cs
@@ -4,29 +4,25 @@ using System.Collections;
 public class Movement : MonoBehaviour 
 {
 	// Beschleunigung in x-, y- und z-Richtung
-	public float accelerationX;
-	public float accelerationY;
-	public float accelerationZ;
+	public Vector3 acceleration;
 
 	// Geschwindigkeit in x-, y- und z-Richtung
-	private float velocityX = 0;
-	private float velocityY = 0;
-	private float velocityZ = 0;
+	private Vector3 velocity;
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		float speedDiffX = accelerationX * Time.deltaTime;
-		velocityX = velocityX + speedDiffX;
+		// speedDiff ist die Änderung der Geschwindigkeit in diesem Frame
+		Vector3 speedDiff = acceleration * Time.fixedDeltaTime;
+		// die velocity (=Geschwindkeit) ändert sich um speedDiff
+		velocity = velocity + speedDiff;
 
-		// Bewegung = Zeitdifferenz * Geschwindigkeit
+		// Bewegung = Geschwindigkeit * vergangene Zeit
 		// s = v * t
-		float deltaX = velocityX * Time.deltaTime;
-		float deltaY = velocityY * Time.deltaTime;
-		float deltaZ = velocityZ * Time.deltaTime;
+		Vector3 posDiff = velocity * Time.fixedDeltaTime;
 
 		// Translate = Bewege um ...
-		this.transform.Translate(deltaX, deltaY, deltaZ);
+		this.transform.Translate(posDiff);
 	}
 }
 
